c953b8339e934e0d95f9812c93d898cdbff98b57
ГорСвет <et224@bk.ru>
2021-11-13 21:31:30 +0300

    Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/calculate/Out.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String separate = " ";
+        String id = idea + separate + "But I am a newbie.";
+        System.out.println(id);
+        int year = 2021;
+        String x = idea + separate + year;
+        System.out.println(x);
+    }
+}
