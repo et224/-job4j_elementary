fa167430197a49901cb4f26716f09460a005646f
ГорСвет <et224@bk.ru>
2021-12-11 20:36:09 +0300

    Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/calculate/math/MathFunction.java
@@ -0,0 +1,11 @@
+package java.ru.job4j.calculate.math;
+
+public class MathFunction {
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+}
