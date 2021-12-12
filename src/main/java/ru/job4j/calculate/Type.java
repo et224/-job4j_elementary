81a479965714c8b10f70e99ccf45d574adcb6f8b
ГорСвет <et224@bk.ru>
2021-12-12 22:18:45 +0300

    Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/calculate/Type.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculate;
+public class Type {
+    public static void main(String[] args) {
+        int num = 1;
+        long b = num;
+        //long numUpper = ...;
+        //short numLower = ...;
+        //System.out.println(numUpper);
+        //System.out.println(numLower);
+    }
+}
