c774f8bf4b1691243d0840b99d600023f9d12346
ГорСвет <et224@bk.ru>
2021-12-26 20:33:06 +0300

    Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -8,6 +8,18 @@ public class MathCalculator {
                 + ru.job4j.math.MathFunction.multiply(first, second);
     }
 
+    public static double diffAndMultiply(double first, double second) {
+        return ru.job4j.math.MathFunction.diff(first, second)
+                + ru.job4j.math.MathFunction.division(first, second);
+    }
+
+    public static double allAndMultiply(double first, double second) {
+        return ru.job4j.math.MathFunction.diff(first, second)
+                + ru.job4j.math.MathFunction.division(first, second)
+                + ru.job4j.math.MathFunction.sum(first, second)
+                + ru.job4j.math.MathFunction.multiply(first, second);
+    }
+
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
     }
