c9e3c38df04da3a0cf94806674667ef848fc8671
et224 <92041970+et224@users.noreply.github.com>
2021-12-29 11:14:31 +0300

    Update Type.java

--- src/main/java/ru/job4j/calculate/Type.java
+++ src/main/java/ru/job4j/calculate/Type.java
@@ -3,9 +3,6 @@ public class Type {
     public static void main(String[] args) {
         int num = 1;
         long b = num;
-        //long numUpper = ...;
-        //short numLower = ...;
-        //System.out.println(numUpper);
-        //System.out.println(numLower);
+       
     }
 }
